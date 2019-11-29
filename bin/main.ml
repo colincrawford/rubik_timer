87d75fee70d272241f63234feda35cdb8ae16709
colinlcrawford <ccolin84@gmail.com>
2019-11-28 19:16:20 -0500

    refactor exe to use let syntax

--- bin/main.ml
+++ bin/main.ml
@@ -3,6 +3,7 @@ open Async
 open Rubik_timer
 
 let stdin = Lazy.force Reader.stdin
+
 let stdout = Lazy.force Writer.stdout
 
 let log_to_stdout s =
@@ -12,42 +13,39 @@ let log_to_stdout s =
 let get_user_input () = Reader.read_line stdin
 
 let rec run_timer_loop score_repo =
-  (log_to_stdout "Ready? ") >>=
-  get_user_input >>= (fun line ->
-      let timer = Timer.start_timer () in
-      match line with
-      | `Eof -> log_to_stdout "EOF!"
-      | `Ok _ -> begin
-          (get_user_input ()) >>= (fun _ ->
-          let timer = Timer.stop_timer timer in
-          let score = Score.of_timer timer in
-          (Score_repository.save_score score_repo score) >>= (fun score ->
-              (Score.to_string score)
-              |> (fun score -> log_to_stdout (score ^ "\n"))) >>= (fun _ ->
-              run_timer_loop score_repo))
-        end)
+  let%bind () = log_to_stdout "Ready? " in
+  let%bind line = get_user_input () in
+  let timer = Timer.start_timer () in
+  match line with
+  | `Eof -> log_to_stdout "EOF!"
+  | `Ok _ ->
+      let%bind _ = get_user_input () in
+      let timer = Timer.stop_timer timer in
+      let score = Score.of_timer timer in
+      let%bind score = Score_repository.save_score score_repo score in
+      let%bind () = log_to_stdout (Score.to_string score ^ "\n") in
+      run_timer_loop score_repo
 
 let create_intro_msg previous_scores =
   let previous_scores_str =
-    (List.map ~f:(fun score -> (Score.to_string score) ^ "\n") previous_scores)
-    |> List.fold ~init:"" ~f:(^) in
+    List.map ~f:(fun score -> Score.to_string score ^ "\n") previous_scores
+    |> List.fold ~init:"" ~f:( ^ )
+  in
   let intro_message =
-    "==========\n" ^
-    "Your Previous Times:\n" ^
-    previous_scores_str ^
-    "==========\n" in
+    "==========\n" ^ "Your Previous Times:\n" ^ previous_scores_str
+    ^ "==========\n"
+  in
   intro_message
-
 
 let main () =
   let score_repo = Score_repository.create () in
-  Score_repository.get_scores score_repo >>= (fun previous_scores ->
-      let intro_message = create_intro_msg previous_scores in
-      (log_to_stdout intro_message) >>= (fun _ ->
-          run_timer_loop score_repo))
+  let%bind previous_scores = Score_repository.get_scores score_repo in
+  let intro_message = create_intro_msg previous_scores in
+  let%bind () = log_to_stdout intro_message in
+  run_timer_loop score_repo
 
-let command = Command.async
-    ~summary:"CLI timer"
+let command =
+  Command.async ~summary:"CLI timer"
     ~readme:(fun () -> "CLI timer")
     (Command.Param.return main)
 
